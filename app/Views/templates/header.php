44dbb63bd91427cfbb5ec31f848d329eabe5b5db
Gilbertsampaio <sampaiogilbert@gmail.com>
2021-09-12 09:12:21 -0300

    titulo

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <title>Carousel Template · Bootstrap v5.1</title>
+    <meta name="description" content="Projeto de novo site">
+    <meta name="author" content="GIlbert Sampaio">
+    <title>Novo Site</title>
     <link href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link href="https://getbootstrap.com/docs/5.1/examples/carousel/carousel.css" rel="stylesheet">
     <link href="<?php echo base_url('../public/css/estilo.css') ?>" rel="stylesheet">
