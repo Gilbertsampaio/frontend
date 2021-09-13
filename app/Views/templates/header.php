5e82d45d49df4aa8b8ca761499a24fb269cb6b33
Gilbertsampaio <sampaiogilbert@gmail.com>
2021-09-12 22:34:28 -0300

    alteração no header

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -3,10 +3,30 @@
 
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Projeto de novo site">
-    <meta name="author" content="GIlbert Sampaio">
-    <title>Novo Site</title>
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@Gilbertsampaio">
+    <meta name="twitter:creator" content="@Gilbertsampaio">
+    <meta name="twitter:title" content="GMELO WEBDESIGNER">
+    <meta name="twitter:description" content="Empresa especializada em desenvolvimento de sistemas WEB e ferramentas administrativas em PHP, JQUERY, CSS, HTML, SQL, AJAX e JAVASCRIPT.">
+    <meta name="twitter:image" content="https://www.gmeloweb.com.br/images/share-miniatura.jpg">
+
+    <title>GMELO WEBDESIGNER - Desenvolvimento de Site.</title>
+    <meta name="author" content="GMELO WEBDESIGNER" />
+    <meta name="description" content="Empresa especializada em desenvolvimento de sistemas WEB e ferramentas administrativas em PHP, JQUERY, CSS, HTML, SQL, AJAX e JAVASCRIPT." />
+    <meta property="og:url" content="https://www.gmeloweb.com.br/" />
+    <meta name="keywords" content="Site, Página, Páginas, Sistema, Sistemas, WEB, PHP, JAVASCRIPT, AJAX, JQUERY, SQL, Book, Portfólio, Blog, Landpage, Onepage, CEO">
+    <meta property="og:title" content="GMELO WEBDESIGNER" />
+    <meta property="og:site_name" content="GMELO WEBDESIGNER">
+    <meta property="og:description" content="Empresa especializada em desenvolvimento de sistemas WEB e ferramentas administrativas em PHP, JQUERY, CSS, HTML, SQL, AJAX e JAVASCRIPT." />
+    <meta property="og:type" content="company">
+    <meta property="og:image" itemprop="image" content="https://www.gmeloweb.com.br/images/share-miniatura.jpg" />
+    <link itemprop="thumbnailUrl" href="https://www.gmeloweb.com.br/images/share-miniatura.jpg">
+    <meta property="og:image:type" content="image/jpeg">
+    <meta property="fb:app_id" content="2069544323344680" />
+    <meta property="fb:admins" content="2069544323344680" />
+    <meta name="google-site-verification" content="EW4i04-XBLPMwriqLdDo49GR94W5L7QtXeuRGeiM3Qq" />
     <link href="https://getbootstrap.com/docs/5.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <link href="https://getbootstrap.com/docs/5.1/examples/carousel/carousel.css" rel="stylesheet">
     <link href="<?php echo base_url('../public/css/estilo.css') ?>" rel="stylesheet">
